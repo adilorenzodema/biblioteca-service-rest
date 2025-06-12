95d04d602b4e032ac8a84d8cbeac92eca0da747a
adilorenzoBenefind <andrea.dilorenzo@benefindgroup.it>
2025-06-12 12:32:22 +0200

    cors

--- src/main/java/com/example/bibliotecaScolastica/configuration/LibriConfiguration.java
+++ src/main/java/com/example/bibliotecaScolastica/configuration/LibriConfiguration.java
@@ -5,16 +5,44 @@ import javax.sql.DataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class LibriConfiguration {
-	   @Bean
-	    public DataSource dataSource() {
-	        return DataSourceBuilder.create()
-	                .driverClassName("org.postgresql.Driver")
-	                .url("jdbc:postgresql://aws-0-eu-west-2.pooler.supabase.com:5432/postgres")
-	                .username("postgres.vstgvcdwlcwlhdqmtwdi")
-	                .password("BibliotecaGiugno")
-	                .build();
-	    }
+	@Bean
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().driverClassName("org.postgresql.Driver")
+				.url("jdbc:postgresql://aws-0-eu-west-2.pooler.supabase.com:5432/postgres")
+				.username("postgres.vstgvcdwlcwlhdqmtwdi").password("BibliotecaGiugno").build();
 	}
+	
+	@Bean
+	public SecurityFilterChain web(HttpSecurity http) throws Exception {
+		http.cors(withDefaults()); // <--- ABILITA CORS
+		http.csrf(AbstractHttpConfigurer::disable);
+		http.authorizeHttpRequests(requests ->
+			requests
+				.requestMatchers("/api/**").permitAll()
+				.anyRequest().denyAll()
+		);
+		return http.build();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.addAllowedOrigin("*");
+	    configuration.addAllowedMethod("*");
+	    configuration.addAllowedHeader("*");
+
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
+	}
+}
