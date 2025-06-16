add42d63e592c49c7cbc958f490e96deca2a0280
adilorenzoBenefind <andrea.dilorenzo@benefindgroup.it>
2025-06-16 16:04:33 +0200

    fix connection

--- src/main/java/com/example/bibliotecaScolastica/configuration/AppConfiguration.java
+++ src/main/java/com/example/bibliotecaScolastica/configuration/AppConfiguration.java
@@ -1,8 +1,7 @@
 package com.example.bibliotecaScolastica.configuration;
 
-import javax.sql.DataSource;
+import static org.springframework.security.config.Customizer.withDefaults;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,16 +10,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class AppConfiguration {
-	@Bean
-	public DataSource dataSource() {
-		return DataSourceBuilder.create().driverClassName("org.postgresql.Driver")
-				.url("jdbc:postgresql://aws-0-eu-west-2.pooler.supabase.com:5432/postgres")
-				.username("postgres.vstgvcdwlcwlhdqmtwdi").password("BibliotecaGiugno").build();
-	}
 	
 	@Bean
 	public SecurityFilterChain web(HttpSecurity http) throws Exception {
